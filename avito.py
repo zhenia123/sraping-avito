0007053a50f3554144a4251a99270a1aac22db08
user <zhenia.aleksandrow2017@yandex.ru>
2023-03-01 15:22:34 +0300

    Изменил значение переменной count

--- avito.py
+++ avito.py
@@ -6,3 +6,4 @@ git commit -a -m 'Изменил значение переменной count'
 git remote add my_clone_project https://v3F2peq@github.com/pylounge/sraping-avito.git
 
 git push my_clone_project pylounge_branch
+dfhgfdkhjkdfkhl;hdfhx
